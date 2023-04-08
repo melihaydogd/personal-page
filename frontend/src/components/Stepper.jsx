204455dd9c4762f480df5d3224b4743720f23f18
Melih <ahaydogdu@hotmail.com>
2023-04-09 00:32:19 +0300

    photo added

--- frontend/src/components/Stepper.jsx
+++ frontend/src/components/Stepper.jsx
@@ -7,49 +7,58 @@ import { ExperienceCard } from "./Card/ExperienceCard";
 import { ProjectCard } from "./Card/ProjectCard";
 
 const springConfig = {
-  duration: "1s",
-  easeFunction: "cubic-bezier(0.15, 0.3, 0.25, 1)",
-  delay: "0s",
+    duration: "1s",
+    easeFunction: "cubic-bezier(0.15, 0.3, 0.25, 1)",
+    delay: "0s",
 };
 
 export const Stepper = (props) => {
-  const theme = useTheme();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const maxSteps = props.cardInfo.length;
+    const theme = useTheme();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const maxSteps = props.cardInfo.length;
 
-  const handleStepChange = (step) => {
-    setActiveStep(step);
-  };
+    const handleStepChange = (step) => {
+        setActiveStep(step);
+    };
 
-  return (
-    <Box sx={{ maxWidth: 320, flexGrow: 1 }}>
-      <SwipeableViews
-        springConfig={springConfig}
-        interval={10000}
-        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents={true}
-        threshold={1}
-      >
-        {props.cardInfo?.map((cardInfo) => {
-          if (props.title === "Experience") {
-            return <ExperienceCard key={cardInfo._id} cardInfo={cardInfo} />
-          } else {
-            return <ProjectCard key={cardInfo._id} cardInfo={cardInfo} />
-          }
-        })}
-      </SwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        sx={{
-          paddingTop: "2rem",
-          justifyContent: "center",
-          backgroundColor: "transparent"
-        }}
-      />
-    </Box>
-  );
+    return (
+        <Box sx={{ maxWidth: 320, flexGrow: 1 }}>
+            <SwipeableViews
+                springConfig={springConfig}
+                axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+                index={activeStep}
+                onChangeIndex={handleStepChange}
+                enableMouseEvents={true}
+                threshold={1}
+            >
+                {props.cardInfo?.map((cardInfo) => {
+                    if (props.title === "Experience") {
+                        return (
+                            <ExperienceCard
+                                key={cardInfo._id}
+                                cardInfo={cardInfo}
+                            />
+                        );
+                    } else {
+                        return (
+                            <ProjectCard
+                                key={cardInfo._id}
+                                cardInfo={cardInfo}
+                            />
+                        );
+                    }
+                })}
+            </SwipeableViews>
+            <MobileStepper
+                steps={maxSteps}
+                position="static"
+                activeStep={activeStep}
+                sx={{
+                    paddingTop: "2rem",
+                    justifyContent: "center",
+                    backgroundColor: "transparent",
+                }}
+            />
+        </Box>
+    );
 };
--- frontend/src/theme.js
+++ frontend/src/theme.js
@@ -3,41 +3,51 @@ import { createTheme } from "@mui/material";
 const lightDefault = "#dfdbcd";
 const blackDefault = "black";
 
-export const lightTheme = createTheme({
-  palette: {
-    mode: "light",
-    background: {
-      default: lightDefault,
-    },
-  },
-  components: {
-    MuiMobileStepper: {
-      styleOverrides: {
-        dotActive: {
-          backgroundColor: blackDefault,
+const MuiAvatar = {
+    styleOverrides: {
+        img: {
+            transform: "scale(1.3)",
         },
-      },
     },
-  },
+};
+
+export const lightTheme = createTheme({
+    palette: {
+        mode: "light",
+        background: {
+            default: lightDefault,
+        },
+    },
+    components: {
+        MuiMobileStepper: {
+            styleOverrides: {
+                dotActive: {
+                    backgroundColor: blackDefault,
+                },
+            },
+        },
+        MuiAvatar,
+    },
 });
 
 export const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-    background: {
-      default: blackDefault,
-    },
-    text: {
-      primary: lightDefault,
-    },
-  },
-  components: {
-    MuiMobileStepper: {
-      styleOverrides: {
-        dotActive: {
-          backgroundColor: lightDefault,
+    palette: {
+        mode: "dark",
+        background: {
+            default: blackDefault,
         },
-      },
+        text: {
+            primary: lightDefault,
+        },
     },
-  },
+    components: {
+        MuiMobileStepper: {
+            styleOverrides: {
+                dotActive: {
+                    backgroundColor: lightDefault,
+                },
+            },
+        },
+        MuiAvatar,
+    },
 });
