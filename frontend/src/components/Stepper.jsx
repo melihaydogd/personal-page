56832e3eb715ff088525321abf095aee3a67f5f2
Melih <ahaydogdu@hotmail.com>
2023-04-09 14:00:26 +0300

    back and next buttons added

--- frontend/src/components/Stepper.jsx
+++ frontend/src/components/Stepper.jsx
@@ -5,60 +5,94 @@ import MobileStepper from "@mui/material/MobileStepper";
 import SwipeableViews from "react-swipeable-views";
 import { ExperienceCard } from "./Card/ExperienceCard";
 import { ProjectCard } from "./Card/ProjectCard";
+import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
+import { Button } from "@mui/material";
 
 const springConfig = {
-    duration: "1s",
-    easeFunction: "cubic-bezier(0.15, 0.3, 0.25, 1)",
-    delay: "0s",
+  duration: "1s",
+  easeFunction: "cubic-bezier(0.15, 0.3, 0.25, 1)",
+  delay: "0s",
 };
 
 export const Stepper = (props) => {
-    const theme = useTheme();
-    const [activeStep, setActiveStep] = React.useState(0);
-    const maxSteps = props.cardInfo?.length;
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = props.cardInfo?.length;
 
-    const handleStepChange = (step) => {
-        setActiveStep(step);
-    };
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
 
-    return (
-        <Box sx={{ maxWidth: 320, flexGrow: 1 }}>
-            <SwipeableViews
-                springConfig={springConfig}
-                axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-                index={activeStep}
-                onChangeIndex={handleStepChange}
-                enableMouseEvents={true}
-                threshold={1}
-            >
-                {props.cardInfo?.map((cardInfo) => {
-                    if (props.title === "Experience") {
-                        return (
-                            <ExperienceCard
-                                key={cardInfo?._id}
-                                cardInfo={cardInfo}
-                            />
-                        );
-                    } else {
-                        return (
-                            <ProjectCard
-                                key={cardInfo?._id}
-                                cardInfo={cardInfo}
-                            />
-                        );
-                    }
-                })}
-            </SwipeableViews>
-            <MobileStepper
-                steps={maxSteps}
-                position="static"
-                activeStep={activeStep}
-                sx={{
-                    paddingTop: "2rem",
-                    justifyContent: "center",
-                    backgroundColor: "transparent",
-                }}
-            />
-        </Box>
-    );
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 320, flexGrow: 1 }}>
+      <SwipeableViews
+        springConfig={springConfig}
+        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents={true}
+        threshold={1}
+      >
+        {props.cardInfo?.map((cardInfo) => {
+          if (props.title === "Experience") {
+            return <ExperienceCard key={cardInfo?._id} cardInfo={cardInfo} />;
+          } else {
+            return <ProjectCard key={cardInfo?._id} cardInfo={cardInfo} />;
+          }
+        })}
+      </SwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        sx={{
+          paddingTop: "2rem",
+          justifyContent: "space-between",
+          backgroundColor: "transparent",
+        }}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+            sx={{
+              "&:hover": { color: theme.palette.grey.A700 },
+            }}
+          >
+            Next
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button
+            size="small"
+            onClick={handleBack}
+            disabled={activeStep === 0}
+            sx={{
+              "&:hover": { color: theme.palette.grey.A700 },
+            }}
+          >
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
+    </Box>
+  );
 };
